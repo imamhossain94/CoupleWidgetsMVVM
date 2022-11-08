64690b957bce26729dd23742552aa35e01220535
imamhossain94 <imamagun94@gmail.com>
2022-11-08 23:37:11 +0600

    feat: DispatcherTimer added to update widget every second

--- CoupleWidgets/Widgets/CoupleWidget.xaml.cs
+++ CoupleWidgets/Widgets/CoupleWidget.xaml.cs
@@ -2,9 +2,11 @@
 using CoupleWidgets.Utils;
 using System;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Threading;
 
 namespace CoupleWidgets.Widgets
 {
@@ -28,7 +30,6 @@ namespace CoupleWidgets.Widgets
             if (e.ButtonState == MouseButtonState.Pressed)
             {
                 helper.updateWidgetPosition(Left, Top);
-
                 DragMove();
             }
         }
@@ -46,17 +47,19 @@ namespace CoupleWidgets.Widgets
             DateTime currentDate = DateTime.Now;
 
             DayCount.Text = currentDate.Subtract(parsedDate).Days.ToString() + " days";
-
             if (coupleData.firstCoupleImage != "")
             {
                 loadCoupleImage(coupleData.firstCoupleImage, FirstImage);
             }
-
             if (coupleData.secondCoupleImage != "")
             {
                 loadCoupleImage(coupleData.secondCoupleImage, SecondImage);
             }
 
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
         }
 
         private void loadCoupleImage(string path, System.Windows.Controls.Image ImageView)
@@ -71,6 +74,7 @@ namespace CoupleWidgets.Widgets
             }
             catch (Exception ex)
             {
+                MessageBox.Show(ex.Message);
                 throw;
             }
         }
@@ -84,5 +88,45 @@ namespace CoupleWidgets.Widgets
         }
 
 
+
+        void DateDiff(DateTime dt1, DateTime dt2)
+        {
+            DateTime zeroTime = new DateTime(1, 1, 1);
+            int leapDaysInBetween = CountLeapDays(dt1, dt2);
+            TimeSpan span = dt2 - dt1;
+            int years = (zeroTime + span).Year - 1;
+            int months = (zeroTime + span).Month - 1;
+            int days = (zeroTime + span).Day - (leapDaysInBetween % 2 == 1 ? 1 : 0);
+            int weeks = days / 7;
+            int remainingdays = days % 7;
+            int hours = (zeroTime + span).Hour;
+            int minutes = (zeroTime + span).Minute;
+            int seconds = (zeroTime + span).Second;
+            DayCount.Text = string.Format("{0}y {1}m {2}d\n{3}h {4}m {5}s", years, months, remainingdays, hours, minutes, seconds);
+        }
+
+        private static int CountLeapDays(DateTime dt1, DateTime dt2)
+        {
+            int leapDaysInBetween = 0;
+            int year1 = dt1.Year, year2 = dt2.Year;
+            DateTime dateValue;
+            for (int i = year1; i <= year2; i++)
+            {
+                if (DateTime.TryParse("02/29/" + i.ToString(), out dateValue))
+                {
+                    if (dateValue >= dt1 && dateValue <= dt2)
+                        leapDaysInBetween++;
+                }
+            }
+            return leapDaysInBetween;
+        }
+
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            DateTime parsedDate = DateTime.Parse(coupleData.startDate);
+            DateTime currentDate = DateTime.Now;
+            DateDiff(parsedDate, currentDate);
+        }
     }
 }
