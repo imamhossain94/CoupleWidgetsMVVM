03cdaa1f24765d179ff507d74ae9825664571f72
imamhossain94 <imamagun94@gmail.com>
2023-02-06 09:05:56 +0600

    feat: Wallpaper previewer created

--- app/src/main/java/com/newagedevs/couplewidgets/view/ui/MainActivity.kt
+++ app/src/main/java/com/newagedevs/couplewidgets/view/ui/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.newagedevs.couplewidgets.view.ui
 
+import android.Manifest
+import android.app.WallpaperManager
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.ImageView
+import androidx.core.app.ActivityCompat
 import com.newagedevs.couplewidgets.R
 import com.newagedevs.couplewidgets.databinding.ActivityMainBinding
 import com.newagedevs.couplewidgets.view.adapter.HeartSymbolAdapter
@@ -22,11 +27,36 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
             symbol = HeartSymbolAdapter()
         }
 
-        val xview:EditText = this.findViewById(R.id.et_your_name)
+        setupPreviewFrame()
+    }
 
-        xview.setText("Imam Hossain")
 
+    private fun setupPreviewFrame() {
+        val preview = findViewById<ImageView>(R.id.bg)
+        val wallpaperManager = WallpaperManager.getInstance(this)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                3
+            )
+            return
+        }
+        val wallpaperDrawable = wallpaperManager.drawable
+        preview.setImageDrawable(wallpaperDrawable)
+    }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        setupPreviewFrame()
     }
 
 
