fed0343f1de4430c700b6423c0bf2a695a347d93
imamhossain94 <imamagun94@gmail.com>
2023-02-10 10:25:44 +0600

    feat: theme color updated

--- app/src/main/java/com/newagedevs/couplewidgets/utils/Constants.kt
+++ app/src/main/java/com/newagedevs/couplewidgets/utils/Constants.kt
@@ -5,7 +5,7 @@ class Constants {
     companion object {
         val feedbackMail = arrayOf("imamagun94@gmail.com")
         val contactMail = arrayOf("imamagun94@gmail.com")
-        const val privacyPolicyUrl = ""
+        const val privacyPolicyUrl = "https://couple-widgets.blogspot.com/2023/02/couple-widget-privacy-policy.html"
         const val sourceCodeUrl = "https://github.com/imamhossain94/CoupleWidgetsMVVM"
         const val publisherName = "https://play.google.com/store/apps/developer?id=NewAgeDevs"
         const val appStoreId = "https://play.google.com/store/apps/details?id=com.newagedevs.couplewidgets"
